45136771b7632cf3a06c81085547e9d19f2919cf
Tony-Kara <eniolaanthony@gmail.com>
2021-08-11 00:05:47 +0600

    Initial Commit

---
+++ Essential developer Project/Essential_developer_Project.h
@@ -0,0 +1,18 @@
+//
+//  Essential_developer_Project.h
+//  Essential developer Project
+//
+//  Created by mac on 8/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Essential_developer_Project.
+FOUNDATION_EXPORT double Essential_developer_ProjectVersionNumber;
+
+//! Project version string for Essential_developer_Project.
+FOUNDATION_EXPORT const unsigned char Essential_developer_ProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Essential_developer_Project/PublicHeader.h>
+
+
