3e1db565f95d98a8ada9e7fb5972bc6fe3d33fd8
Tony-Kara <eniolaanthony@gmail.com>
2021-08-11 00:14:17 +0600

    Create the FeedItem model

---
+++ Essential developer Project/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Essential developer Project
+//
+//  Created by mac on 8/11/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String
+    let imageURL: URL
+}
