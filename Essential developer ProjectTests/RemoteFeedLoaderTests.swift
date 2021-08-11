7088e37286517181d35361860c118bb961412662
Tony-Kara <eniolaanthony@gmail.com>
2021-08-11 19:54:15 +0600

    Make the HTTPClient a protocol

--- Essential developer ProjectTests/RemoteFeedLoaderTests.swift
+++ Essential developer ProjectTests/RemoteFeedLoaderTests.swift
@@ -8,25 +8,28 @@
 import XCTest
 
 class RemoteFeedLoader {
+    
+    var client : HTTPClient
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load() {
-        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
     // we do not want to requestedURL from HTTPClient in production code, this is for test purpose only, changing the singleton from let to var
     // ensure we have other possiblity which is to create a subclass of the http client, so we move the test logic from HTTPClient to HTTPClientSpy
-class HTTPClient {
-    static var shared = HTTPClient()
+protocol HTTPClient {
    
-    
-    func get(from url: URL) {}
-    
+    func get(from url: URL)
 
 }
 
 
 class HTTPCLientSpy: HTTPClient {
-    override func get(from url: URL){
+     func get(from url: URL){
         requestedURL = url
     }
     var requestedURL: URL?
@@ -36,8 +39,8 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPCLientSpy()
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+       
+        _ = RemoteFeedLoader(client: client)
         XCTAssertNil(client.requestedURL)
         
     }
@@ -46,9 +49,8 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_RequestDataFromURL(){
         // the idea is to request for URL throught the client
         let client = HTTPCLientSpy()
-        // overiride the shared property
-        HTTPClient.shared = client
-        let sut = RemoteFeedLoader()
+        
+        let sut = RemoteFeedLoader(client: client)
         
         sut.load()
         XCTAssertNotNil(client.requestedURL)
