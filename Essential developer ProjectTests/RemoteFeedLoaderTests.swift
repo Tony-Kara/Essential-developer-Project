d2996457f2c79c9afe31d007b99f32ee5e5d3390
Tony-Kara <eniolaanthony@gmail.com>
2021-08-11 23:56:19 +0600

    Refractor to remove duplication

--- Essential developer ProjectTests/RemoteFeedLoaderTests.swift
+++ Essential developer ProjectTests/RemoteFeedLoaderTests.swift
@@ -31,19 +31,14 @@ protocol HTTPClient {
 }
 
 
-class HTTPCLientSpy: HTTPClient {
-     func get(from url: URL){
-        requestedURL = url
-    }
-    var requestedURL: URL?
-}
+
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPCLientSpy()
-        let url = URL(string: "https://a-url.com")!
-        _ = RemoteFeedLoader(url: url ,client: client)
+        
+       
+        let (_ , client) = makeSUT()
         XCTAssertNil(client.requestedURL)
         
     }
@@ -51,15 +46,28 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_RequestDataFromURL(){
         // the idea is to request for URL throught the client
-        let client = HTTPCLientSpy()
+        
         let url = URL(string: "https://a-given-url.com")
-        let sut = RemoteFeedLoader(url: url! , client: client)
+        let (sut, client)  = makeSUT(url: url!)
         
         sut.load()
         XCTAssertEqual(client.requestedURL, url)
         
     }
     
+                                                                            //this is a turple, and i have to return it
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPCLientSpy) {
+        
+        let client = HTTPCLientSpy() //created inside the function which means there is no need to pass it as an argument
+        let sut =  RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
     
-    
+   private class HTTPCLientSpy: HTTPClient { // this will not be part of production code
+    var requestedURL: URL?
+    func get(from url: URL){
+            requestedURL = url
+        }
+       
+    }
 }
